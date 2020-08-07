9fb520ff7348cb3ba703ec5440cc60eee5f9e374
nssimida <tomufeng@163.com>
2020-08-07 09:30:13 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 // Your function must pass all the tests at: https://github.com/nssimida/test-demo/blob/master/test/index.test.ts. 
 // Once you have finished, you can run `npm start` to test.
+
+/**
+ * Transform all eligible objects into an array
+ * 
+ * @param obj
+ */
 export function transform<T = string | number | boolean>(obj: Record<string, T | T[]>): Record<string, T>[] {
 }
